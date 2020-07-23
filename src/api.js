b28a3dbdf12d91d62a97f8334f26f7ef6f4165a1
vincent0122 <38850727+vincent0122@users.noreply.github.com>
2020-07-23 17:20:48 +0900

    Update api.js

--- src/api.js
+++ src/api.js
@@ -83,7 +83,7 @@ apiRouter.post('/showHello', function(req, res) {
           //  imageUrl: "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
           //  altText: "HELLO"
           simpleText: {
-            text:  
+            text: x
            
           }
         }
