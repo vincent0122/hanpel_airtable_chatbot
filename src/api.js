05ac740a8a47cd7172b1ddd873e9331897a2fca1
vincent0122 <38850727+vincent0122@users.noreply.github.com>
2020-08-06 18:36:27 +0900

    Update api.js

--- src/api.js
+++ src/api.js
@@ -74,47 +74,47 @@ apiRouter.post("/air_input_m", async (req, res) => {
   var contents = content.replace(/\"/g, "");
   var wri = writer.replace(/\"/g, "");
 
-  // switch (wri) {
-  //   case "2c2e571aa09087b61c573115011b68b41683e3634ca15ee80f7fb14c44765c4343":
-  //     var wri2 = "임진석";
-  //     break;
-  //
-  //   case "4f7a76d5ca088a897215c06004e3c9ef729b0af9b6d2e682c3abfea680c3114b28":
-  //     var wri2 = "임진강";
-  //     break;
-  //
-  //   case "44ad8cc1cfa54644ca2b31d066884fe56e6e7f62e874f88d3d2b9031f286a7404b":
-  //     var wri2 = "나준호";
-  //     break;
-  //
-  //   case "1cf2d917e4daaf63a762a8cb1db925e0bc70559fa1bf9e1fc5bdb6fb5a426f8e30":
-  //     var wri2 = "정소영";
-  //     break;
-  //
-  //   case "7ade03d9eb16a38f45cc8f5855a368ad6a755f8480d80f794fee2631732514e1c0":
-  //     var wri2 = "송혜주";
-  //     break;
-  //
-  //   case "66403b9819685f49eb67931a9d6ac55cde1cd5107fc47c33ee04d3b2ae710348b5":
-  //     var wri2 = "이선화";
-  //     break;
-  //
-  //   case "13ee3d86de309d4ba5bc27f1fe8809747caac66c710e60f0dd5ad65d9dd36f125b":
-  //     var wri2 = "추승혜";
-  //     break;
-  //
-  //   case "37f5153c8f36e0fe585605f71d2cf07ab609d58d2ad8b0e3d458b2f50b3bdb6778":
-  //     var wri2 = "심동현";
-  //     break;
-  //
-  //   case "00ecadf0b098f5a281c6995ca8976a5b78e6981d3f958f953c6c5ef3b879032743":
-  //     var wri2 = "임진아";
-  //     break;
-  //
-  //   case "89686eb7290a335a1314eb25c5d977a2eeb1826d84ee28bc599a2a65a57fd7ee34":
-  //     var wri2 = "강대현";
-  //     break;
-  // }
+  switch (wri) {
+    case "2c2e571aa09087b61c573115011b68b41683e3634ca15ee80f7fb14c44765c4343":
+      var wri2 = "임진석";
+      break;
+  
+    case "4f7a76d5ca088a897215c06004e3c9ef729b0af9b6d2e682c3abfea680c3114b28":
+      var wri2 = "임진강";
+      break;
+  
+    case "44ad8cc1cfa54644ca2b31d066884fe56e6e7f62e874f88d3d2b9031f286a7404b":
+      var wri2 = "나준호";
+      break;
+  
+    case "1cf2d917e4daaf63a762a8cb1db925e0bc70559fa1bf9e1fc5bdb6fb5a426f8e30":
+      var wri2 = "정소영";
+      break;
+  
+    case "7ade03d9eb16a38f45cc8f5855a368ad6a755f8480d80f794fee2631732514e1c0":
+      var wri2 = "송혜주";
+      break;
+  
+    case "66403b9819685f49eb67931a9d6ac55cde1cd5107fc47c33ee04d3b2ae710348b5":
+      var wri2 = "이선화";
+      break;
+  
+    case "13ee3d86de309d4ba5bc27f1fe8809747caac66c710e60f0dd5ad65d9dd36f125b":
+      var wri2 = "추승혜";
+      break;
+  
+    case "37f5153c8f36e0fe585605f71d2cf07ab609d58d2ad8b0e3d458b2f50b3bdb6778":
+      var wri2 = "심동현";
+      break;
+  
+    case "00ecadf0b098f5a281c6995ca8976a5b78e6981d3f958f953c6c5ef3b879032743":
+      var wri2 = "임진아";
+      break;
+  
+    case "89686eb7290a335a1314eb25c5d977a2eeb1826d84ee28bc599a2a65a57fd7ee34":
+      var wri2 = "강대현";
+      break;
+  }
 
   await base("총무,수출입").create({
     Attachments: [
@@ -145,7 +145,7 @@ apiRouter.post("/air_input_m", async (req, res) => {
   res.status(200).send(responseBody);
 });
 
-apiRouter.post("/air_input_pc", function (req, res) {
+apiRouter.post("/air_input_pc", (req, res) => {
   var pic = JSON.stringify(req.body.action.detailParams.file.origin);
   item.set_arr(pic);
   var pic2 = item.get_arr();
@@ -153,18 +153,50 @@ apiRouter.post("/air_input_pc", function (req, res) {
 
   const responseBody = {
     version: "2.0",
+    data: {
+      msg: "HI",
+      name: "Ryan",
+      position: "Senior Managing Director",
+    },
     template: {
       outputs: [
         {
-          simpleText: {
-            text: pic + "업로드(계속 진행하세요)" + pic2,
+          basicCard: {
+            title: "보물상자",
+            description: "보물상자 안에는 뭐가 있을까",
+            thumbnail: {
+              imageUrl:
+                "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg",
+            },
+            profile: {
+              imageUrl:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4BJ9LU4Ikr_EvZLmijfcjzQKMRCJ2bO3A8SVKNuQ78zu2KOqM",
+              nickname: "보물상자",
+            },
+            social: {
+              like: 1238,
+              comment: 8,
+              share: 780,
+            },
+            buttons: [
+              {
+                action: "message",
+                label: "열어보기",
+                messageText: "짜잔! 우리가 찾던 보물입니다",
+              },
+              {
+                action: "block",
+                label: "입력하기",
+                blockId: "sf3gmjguuel553rm6vk4qp0e",
+              },
+            ],
           },
         },
       ],
     },
   };
-
-  res.status(200).send(responseBody).then();
+  //
+  res.status(200).send(responseBody);
 });
 
 apiRouter.post("/air_input_pc_f", async (req, res) => {
